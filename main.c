fab88f4bc07f80273d2f3f49e71e96c52794f105
Michael Banack <bob5972@banack.net>
2011-11-29 20:14:58 -0800

    It works!

--- main.c
+++ main.c
@@ -1,10 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "random.h"
 
-int main()
+int main(int argc, char *argv[])
 {
-	printf("Hello world!\n");
+	int min, max;
+	Random_Init();
+	
+	//printf("argc = %d\n", argc);
+	if (argc != 3) {
+		printf("Usage: random min max\n");
+		goto done;
+	}
+	
+	// argv[0] is the program name
+	min = atoi(argv[1]);
+	max = atoi(argv[2]);
+	
+	//printf("min = %d\n", min);
+	//printf("max = %d\n", max);
+	
+	if (min > max) {
+		int temp = min;
+		min = max;
+		max = temp;
+	}
+	
+	printf("%d\n", Random_Int(min, max));
+	
+done:
+	Random_Exit();
 	return 0;
 }
 
